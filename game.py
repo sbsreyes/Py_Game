69afeb6664796563f4ba820d62f1270bbe035bd3
Sebastian Reyes <sbsreyes.js@gmail.com>
2020-04-25 18:00:01 -0500

    Se crea el player y se realiza el movimiento natural de caida

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import pygame
 end = False
 
 bg_color = (156, 156, 156)
-
+player_color = (0,0,0)
 pygame.init()
 
 window = 700,500
@@ -14,13 +14,32 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode(window)
 
+x = 350
+y = 250
+y_vel = 0
+
+def player(x, y):
+    pygame.draw.circle(screen, player_color, [x, y], 20)
+
 while not end:
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             end = True
 
+    if event.type == pygame.KEYDOWN:
 
-    screen.fill((155,155,155))
+        if event.key == pygame.K_UP:
+            y_vel = -15
+
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_UP:
+            y_vel = 5
+
+
+    screen.fill(bg_color)
+    player(x, y)
+    y = y + y_vel
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
